fc0a00d323ddc9e1e76e5a65bc8a3b18e08440d6
eizus <hello@cdr.xyz>
2026-06-22 17:26:33 +0000

    blastshield 0.1.11

--- Formula/blastshield.rb
+++ Formula/blastshield.rb
@@ -4,9 +4,9 @@
 class Blastshield < Formula
   desc "Sandbox AI coding agents against destructive cloud CLI commands"
   homepage "https://cdrxyz.github.io/blastshield"
-  url "https://github.com/cdrxyz/blastshield/releases/download/v0.1.10/blastshield-0.1.10.tar.gz"
-  version "0.1.10"
-  sha256 "74073eecb45a2687ab8a63925d1ec7245c131da0812a5270353973edcf068a68"
+  url "https://github.com/cdrxyz/blastshield/releases/download/v0.1.11/blastshield-0.1.11.tar.gz"
+  version "0.1.11"
+  sha256 "7ddd4a0354aeae1dcd39ec5ca0bbb3d53aefcf307c227014f9a3520607954dd9"
   license "Apache-2.0"
 
   depends_on "bash"
